1165a37a4d3e40df7651c80e4292228aa0d8677e
SouLeo <slwanna@utexas.edu>
2025-03-03 18:57:38 -0700

    fixed file size in tacoplay

--- download_scripts/download_taco_play.sh
+++ download_scripts/download_taco_play.sh
@@ -5,7 +5,7 @@ PYTHON_PKG_LOC=`dirname \`pwd\``
 DS_DOWNLOAD_DIR=${PYTHON_PKG_LOC%%/}/downloaded_datasets/
 
 echo "This download assumes you are running the script from download_scripts. Please cancel if not."
-echo "WARNING: This dataset is 111.1 GB."
+echo "WARNING: This dataset is 47.77 GB."
 echo "This will download to $DS_DOWNLOAD_DIR "
 
 sleep 30
